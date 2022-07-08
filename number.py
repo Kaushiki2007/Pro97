143633e0845ddf0edad85830f105a0b45fd20e2a
Kaushiki2007 <79528338+Kaushiki2007@users.noreply.github.com>
2022-07-09 01:31:32 +0530

    Add files via upload

---
+++ number.py
@@ -0,0 +1,11 @@
+number = 9
+chances = 5
+guess = chances
+while chamces < 5
+
+if guess == number :
+    print("CONGRATULATIONS ! YOU WON")
+    break
+
+if not chances > 5:
+    print("YOU LOSE! THE NUMBER IS 9")
